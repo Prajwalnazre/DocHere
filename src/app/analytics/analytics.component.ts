1583d0c86b6ee5547c0fe212d763f6aab5457e2a
Prajwalnazre <prajwal.nazre777@gmail.com>
2021-03-09 13:37:36 +0530

    Line Graph added

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType } from 'chart.js';
-import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip } from 'ng2-charts';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip, Color } from 'ng2-charts';
  
 
 @Component({
@@ -10,14 +10,36 @@ import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsToolt
 })
 export class AnalyticsComponent implements OnInit {
 
+  //Pie Chart Start
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
   public pieChartLabels: Label[] = ['Women', 'Men'];
-  public pieChartData: SingleDataSet = [30, 50];
+  public pieChartData: SingleDataSet = [65, 80];
   public pieChartType: ChartType = 'pie';
   public pieChartLegend = true;
   public pieChartPlugins = [];
+
+  // Pie Chart End
+
+  public lineChartData: ChartDataSets[] = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Age', backgroundColor: 'rgba(0, 194, 211, 1)', borderColor: 'fillPattern',
+    pointBackgroundColor: 'fillPattern'},
+  ];
+  public lineChartLabels: Label[] = ['1', '2', '3', '4', '5', '6', '7'];
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+
+  };
+  public lineChartColors: Color[] = [
+    {
+      borderColor: 'blue',
+      backgroundColor: 'rgba(110,0,0,0.3)',
+    },
+  ];
+  public lineChartLegend = true;
+  public lineChartType : ChartType = 'line';
+  public lineChartPlugins = [];
  
   constructor() {
     monkeyPatchChartJsTooltip();
